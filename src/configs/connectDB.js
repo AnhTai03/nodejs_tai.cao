1fda179137367f453772018dcf932b5e229c584b
Anhtai03 <anhtai2492003@gmail.com>
2022-07-08 10:57:37 +0700

    REQUEST METHOD & SỬ DỤNG DATABASE VỚI NODE.JS

---
+++ src/configs/connectDB.js
@@ -0,0 +1,23 @@
+// get the client
+
+import mysql from 'mysql2';
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'nodejsbase',
+});
+
+// // simple query
+// connection.query('SELECT * FROM `user` ', function (err, results, fields) {
+//   console.log('>>> check mysql');
+//   console.log(results); // results contains rows returned by server
+//   let rows = results.map((row) => {
+//     return row.id;
+//   });
+
+//   console.log(rows); // fields contains extra meta data about results, if available
+// });
+
+export default connection;
---
+++ src/controller/homeController.js
@@ -0,0 +1,30 @@
+import connection from '../configs/connectDB';
+
+let getHomePage = (req, res) => {
+  // logic
+  let data = [];
+  connection.query('SELECT * FROM `user` ', function (err, results, fields) {
+    console.log('>>> check mysql');
+    console.log(results); // results contains rows returned by server
+    results.map((row) => {
+      data.push({
+        id: row.id,
+        firstName: row.firstName,
+        lastName: row.lastName,
+        email: row.email,
+        address: row.address,
+      });
+    });
+    return res.render('test/index.ejs', { dataUser: JSON.stringify(data) });
+  });
+};
+
+let getProductPage = (req, res) => {
+  //logic
+  return res.render('element/product.ejs');
+};
+
+module.exports = {
+  getHomePage,
+  getProductPage,
+};
